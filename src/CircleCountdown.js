ad85988bf08c2920fba1cdb2f9363ae4e20a5fdf
Riccardo Attilio Galli <riccardo@sideralis.org>
2018-05-21 19:26:50 -0700

    Draw the arc using a saved conic gradient

--- src/CircleCountdown.js
+++ src/CircleCountdown.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 
 import { STATUS_PLAY, STATUS_STOP, STATUS_PAUSE } from './constants';
 
+const PI = Math.PI; // cache pi
+
 class CircleCountdown extends Component {
   constructor(props) {
     super();
     this.resetInternalCounters();
-    this.state = {};
+    this.state = {
+      sideLength: null,
+      radius: null,
+      fontSize: null
+    };
   }
   resetInternalCounters = () => {
     this.textUpdatedAt = 0;
@@ -18,14 +24,10 @@ class CircleCountdown extends Component {
 
     if (nextSideLength !== prevState.sideLength) {
       nextState.sideLength = nextSideLength;
-      nextState.radius = nextSideLength / 2 * 0.9; // XXX is 0.9 necessary?
+      nextState.radius = nextSideLength / 2 * 0.9;
     }
 
     nextState.fontSize = nextProps.getFontSize(nextSideLength);
-
-    if (nextProps.colorLineForeground !== prevState.colorLineForeground) {
-      nextState.colorLineForeground = nextProps.colorLineForeground;
-    }
 
     return nextState;
   }
@@ -34,7 +36,8 @@ class CircleCountdown extends Component {
       nextProps.width !== this.props.width ||
       nextProps.height !== this.props.height ||
       nextProps.className !== this.props.className ||
-      nextProps.status !== this.props.status
+      nextProps.status !== this.props.status ||
+      nextProps.colorLineForeground !== this.props.colorLineForeground
     );
   };
   setCircleCanvas = elem => {
@@ -45,7 +48,7 @@ class CircleCountdown extends Component {
   };
   componentDidMount() {
     this.circleContext = this.circleCanvas.getContext('2d');
-    this.circleContext.lineCap = 'round';
+    this.circleContext.lineCap = 'butt';
     this.circleContext.scale(window.devicePixelRatio, window.devicePixelRatio);
 
     this.textCanvasContext = this.textCanvas.getContext('2d');
@@ -57,27 +60,46 @@ class CircleCountdown extends Component {
       window.devicePixelRatio
     );
 
-    this.colors = [...this.state.colorLineForeground];
-  }
-  componentDidUpdate(prevProps, prevState) {
-    this.colors = [...this.state.colorLineForeground];
-  }
-  getColors(timePassedAspercent) {
-    timePassedAspercent = timePassedAspercent / 100;
+    const colorsConf = this.getColors();
+    for (let i = 0, il = colorsConf.length; i < il; i++) {
+      let value = colorsConf[i];
+      const startDegree = value[0],
+        // Draw every slice one degree greater but let the nexst slide start
+        // at the correct position so they overlap. This will fix some random
+        // gaps that are caused by antialiasing.
+        endDegree = value[1] + (i === il - 1 ? 0 : 1),
+        startColor = value[2],
+        endColor = value[3];
 
-    while (
-      timePassedAspercent > this.colors[0][1] &&
-      timePassedAspercent <= 1
-    ) {
-      this.colors = this.colors.slice(1);
+      drawConicGradient(
+        this.circleContext,
+        this.state.sideLength,
+        startColor,
+        endColor,
+        startDegree,
+        endDegree
+      );
     }
-
-    const colors = this.colors;
-    const nextColorAlpha =
-      (timePassedAspercent - colors[0][0]) / (colors[0][1] - colors[0][0]);
-    return colors[0][2] === colors[0][3]
-      ? [[colors[0][2], 1]]
-      : [[colors[0][2], 1 - nextColorAlpha], [colors[0][3], nextColorAlpha]];
+    this.conicGradientData = this.circleContext.getImageData(
+      0,
+      0,
+      this.circleCanvas.width,
+      this.circleCanvas.width
+    );
+    this.circleContext.clearRect(
+      0,
+      0,
+      this.state.sideLength,
+      this.state.sideLength
+    );
+  }
+  getColors() {
+    const out = [];
+    for (let i = 0; i < this.props.colorLineForeground.length; i++) {
+      const row = this.props.colorLineForeground[i];
+      out.push([row[0] * 360, row[1] * 360, row[2], row[3]]);
+    }
+    return out;
   }
   draw({
     remainingMillis,
@@ -91,8 +113,6 @@ class CircleCountdown extends Component {
     status
   }) {
     const timePassedAspercent = 100 - remainingMillis * 100 / duration;
-    const startDegree = this.props.startDegree + 270;
-    const endDegree = startDegree + timePassedAspercent * 360 / 100;
     let text = null;
 
     if (
@@ -106,7 +126,7 @@ class CircleCountdown extends Component {
       text = getTimeText(Math.max(0, remainingMillis), duration, tickInterval);
     }
 
-    this.drawFrame(startDegree, endDegree, text, status, timePassedAspercent);
+    this.drawFrame(text, status, timePassedAspercent);
   }
   drawReset({ remainingMillis, duration, tickInterval, status }) {
     const timePassedAspercent = 100 - remainingMillis * 100 / duration;
@@ -118,41 +138,73 @@ class CircleCountdown extends Component {
       tickInterval
     );
 
-    this.drawFrame(startDegree, endDegree, text, status, timePassedAspercent);
-  }
-  drawFrame(startDegree, endDegree, text, status, timePassedAspercent) {
-    gradientLineForeground;
+    this.circleContext.clearRect(
+      0,
+      0,
+      this.state.sideLength,
+      this.state.sideLength
+    );
+    this.circleContext.putImageData(
+      this.conicGradientData,
+      0,
+      0,
+      0,
+      0,
+      this.circleCanvas.width,
+      this.circleCanvas.height
+    );
 
+    this.circleContext.beginPath();
+    const halfSide = this.state.sideLength / 2;
+    const radiusExt = halfSide * 0.9; // external circle radiuse
+    const radiusInt = radiusExt * 0.7; // inner circle radius
+    this.circleContext.arc(
+      halfSide,
+      halfSide,
+      radiusInt + (radiusExt - radiusInt) / 2,
+      0,
+      2 * PI
+    );
+    this.circleContext.strokeStyle = this.props.colorLineBackgroundInactive;
+    this.circleContext.lineWidth = radiusExt - radiusInt;
+    this.circleContext.stroke();
+  }
+  drawFrame(text, status, timePassedAspercent) {
     const circleContext = this.circleContext;
     const halfSide = this.state.sideLength / 2;
-    const radius = this.state.radius;
-    const PI = 245850922 / 78256779;
+    const radius1 = this.state.radius;
+    const endArc = 2 * PI * timePassedAspercent / 100;
 
+    circleContext.globalCompositeOperation = 'source-over';
+    circleContext.clearRect(0, 0, this.state.sideLength, this.state.sideLength);
+    circleContext.putImageData(
+      this.conicGradientData,
+      0,
+      0,
+      0,
+      0,
+      this.circleCanvas.width,
+      this.circleCanvas.width
+    );
     circleContext.beginPath();
-    circleContext.arc(halfSide, halfSide, radius, 0, 2 * PI);
+
+    const radius2 = radius1 * 0.7; // inner circle radius
+
+    circleContext.arc(
+      halfSide,
+      halfSide,
+      radius2 + (radius1 - radius2) / 2,
+      endArc,
+      2 * PI
+    );
+
     circleContext.strokeStyle =
       status === STATUS_PLAY
         ? this.props.colorLineBackgroundActive
         : this.props.colorLineBackgroundInactive;
-    circleContext.lineWidth = radius / 10;
+
+    circleContext.lineWidth = radius1 - radius2;
     circleContext.stroke();
-
-    if (timePassedAspercent > 1) {
-      for (let value of this.getColors(timePassedAspercent)) {
-        const color = value[0],
-          alpha = value[1];
-        const startRadians = startDegree * PI / 180;
-        const endRadians = endDegree * PI / 180;
-
-        circleContext.beginPath();
-        circleContext.lineWidth = radius / 10;
-        circleContext.globalAlpha = alpha;
-        circleContext.strokeStyle = color;
-        circleContext.arc(halfSide, halfSide, radius, startRadians, endRadians);
-        circleContext.stroke();
-        circleContext.globalAlpha = 1;
-      }
-    }
 
     text !== null &&
       drawTextInCircle(
@@ -319,3 +371,99 @@ function getTimeText(remainingMillis, duration, tickInterval) {
 }
 
 export default CircleCountdown;
+
+function drawConicGradient(
+  ctx,
+  sideLength,
+  startColor,
+  endColor,
+  startDegree,
+  endDegree
+) {
+  const pieces = 120;
+  const degrees = endDegree - startDegree; // XXX will not work with higher than 360 degrees
+  const centerX = sideLength / 2; // x center of circle
+  const centerY = sideLength / 2; // y center of circle
+  const size = degrees / pieces; // size of pie slice in degrees
+  const radius1 = sideLength / 2 * 0.9; // outer circle radius
+  const radius2 = radius1 * 0.7; // inner circle radius
+
+  // draw the clipping arc
+  const startRadians = Math.round(startDegree * PI / 180 * 100) / 100;
+  const endRadians = Math.round(endDegree * PI / 180 * 100) / 100;
+
+  const color1 = cssColorToRGBA(startColor);
+  const color2 = cssColorToRGBA(endColor);
+
+  // draw the big circle
+  ctx.globalCompositeOperation = 'source-over';
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius1, startRadians, endRadians, false);
+  ctx.lineTo(centerX, centerY);
+  ctx.fill();
+
+  // knock out the little circle from the big circle
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.beginPath();
+  if (radius2 > radius1) {
+    radius2 = radius1;
+  }
+  ctx.arc(centerX, centerY, radius2, 0, PI * 2, false);
+  ctx.lineTo(centerX, centerY);
+  ctx.fill();
+
+  // Draw as many slices as needed with gradient colors
+  ctx.globalCompositeOperation = 'source-atop';
+
+  for (let i = 0; i < pieces; i++) {
+    const deg = i * size + startDegree;
+    const red = parseInt(
+      (color2[0] - color1[0]) * i / (pieces - 1) + color1[0],
+      10
+    );
+    const green = parseInt(
+      (color2[1] - color1[1]) * i / (pieces - 1) + color1[1],
+      10
+    );
+    const blue = parseInt(
+      (color2[2] - color1[2]) * i / (pieces - 1) + color1[2],
+      10
+    );
+    ctx.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    const x1 = centerX + radius1 * 10 * Math.cos(deg * PI / 180);
+    const y1 = centerY + radius1 * 10 * Math.sin(deg * PI / 180);
+    // Draw every slice one degree greater but let the nexst slide start
+    // at the correct position so they overlap. This will fix some random
+    // gaps that are caused by antialiasing.
+    const extraDeg = i === pieces - 1 ? 0 : 1;
+    const x2 =
+      centerX + radius1 * 10 * Math.cos((deg + size + extraDeg) * PI / 180);
+    const y2 =
+      centerY + radius1 * 10 * Math.sin((deg + size + extraDeg) * PI / 180);
+    ctx.lineTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.fill();
+  }
+
+  ctx.globalCompositeOperation = 'source-over';
+}
+
+function cssColorToRGBA(cssColor) {
+  const d = document.createElement('div');
+  d.style.color = cssColor;
+  document.body.appendChild(d);
+  //Color in RGB
+  const rgbString = window.getComputedStyle(d).color;
+  d.remove();
+  const parsed = rgbString.match(
+    /rgb[a]?\((\d+),\s*(\d+),\s*(\d+)(,\s*(\d+(\.\d+)?))?\)/
+  );
+  return [
+    parseInt(parsed[1], 10),
+    parseInt(parsed[2], 10),
+    parseInt(parsed[3], 10),
+    parseFloat(Math.round(parsed[5] * 100) / 100 || 1)
+  ];
+}
